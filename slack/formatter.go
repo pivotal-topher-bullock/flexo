fb8603a9bdce7dec180838af85353b15b89e56e3
Topher Bullock <cbullock@pivotal.io>
2017-03-20 13:32:55 -0400

    tests for slack formatter
    Signed-off-by: Topher Bullock <cbullock@pivotal.io>

--- slack/formatter.go
+++ slack/formatter.go
@@ -2,6 +2,7 @@ package slack
 
 import (
 	"github.com/pivotal-topher-bullock/flexo"
+	"fmt"
 )
 
 func NewSlackFormatter(config flexo.FormatterConfig) flexo.Formatter {
@@ -23,23 +24,23 @@ func (sf *slackFormatter) Format(tokens <-chan flexo.Token) []string {
 }
 
 func (sf *slackFormatter) formatToken(token flexo.Token) string {
-	// switch token.Type {
-	// case flexo.TextToken:
-	// 	return token.Content
-	// case flexo.LinkStartToken:
-	// 	return fmt.Sprintf("<%s%s|", sf.config.LinkPrefix, token.Attributes["href"])
-	// case flexo.LinkEndToken:
-	// 	return ">"
-	// case flexo.ListItemStartToken:
-	// 	return "- "
-	// case flexo.ListStartToken:
-	// 	fallthrough
-	// case flexo.ListEndToken:
-	// 	fallthrough
-	// case flexo.ListItemEndToken:
-	// 	return "\n"
-	// default:
-	// 	return ""
-	// }
+	 switch token.Type {
+	 case flexo.TextToken:
+		return token.Content
+	 case flexo.LinkStartToken:
+		return fmt.Sprintf("<%s%s|", sf.config.LinkPrefix, token.Attributes["href"])
+	 case flexo.LinkEndToken:
+		return ">"
+	 case flexo.ListItemStartToken:
+		return "- "
+	 case flexo.ListStartToken:
+		fallthrough
+	 case flexo.ListEndToken:
+		fallthrough
+	 case flexo.ListItemEndToken:
+		return "\n"
+	 default:
+		return ""
+	 }
 	return ""
 }
--- slack/formatter_test.go
+++ slack/formatter_test.go
@@ -19,14 +19,84 @@ var _ = Describe("Formatter", func() {
 	})
 
 	Describe("Run", func() {
-
 		var (
 			tokenChannel chan flexo.Token
+			tokens []flexo.Token
+			output []string
 		)
 
 		JustBeforeEach(func() {
 			tokenChannel = make(chan flexo.Token)
-			lexer.Format(tokenChannel)
+			go func() {
+				for _, token := range tokens {
+					tokenChannel <- token
+				}
+				close(tokenChannel)
+			}()
+
+
+			output = formatter.Format(tokenChannel)
+		})
+
+		Context("parsing link tags", func() {
+			BeforeEach(func() {
+				tokens = []flexo.Token{
+					{
+						Type: flexo.LinkStartToken,
+						Attributes: map[string]string{ "href" : "example.com"},
+					},
+					{
+						Type: flexo.TextToken,
+						Content : "a slack link",
+					},
+					{
+						Type: flexo.LinkEndToken,
+					},
+				}
+			})
+
+			It("builds the proper string for a slack message link", func() {
+				Expect(len(output)).To(Equal(3))
+				Expect(output).To(BeEquivalentTo([]string{"<example.com|","a slack link",">"}))
+			})
+		})
+
+		Context("parsing list items", func() {
+			BeforeEach(func() {
+				tokens = []flexo.Token{
+					{
+						Type: flexo.ListStartToken,
+					},
+					{
+						Type: flexo.ListItemStartToken,
+					},
+					{
+						Type: flexo.TextToken,
+						Content : "one",
+					},
+					{
+						Type: flexo.ListItemEndToken,
+					},
+					{
+						Type: flexo.ListItemStartToken,
+					},
+					{
+						Type: flexo.TextToken,
+						Content : "two",
+					},
+					{
+						Type: flexo.ListItemEndToken,
+					},
+					{
+						Type: flexo.ListEndToken,
+					},
+				}
+			})
+
+			It("builds the proper string for a slack message link", func() {
+				Expect(len(output)).To(Equal(8))
+				Expect(output).To(BeEquivalentTo([]string{"\n","- ","one","\n","- ","two","\n","\n"}))
+			})
 		})
 	})
 })
